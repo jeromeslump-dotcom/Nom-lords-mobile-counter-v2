a289837eaee1343eef302f7bba731cc4cd84d14a
jeromeslump-dotcom <jeromeslump@gmail.com>
2026-08-19 15:36:57 +0200

    refactor: add utils barrel exports

---
+++ src/utils/index.ts
@@ -0,0 +1,10 @@
+export { TYPE_GRADIENT, getHeroTypeGradient } from "./heroTypes";
+export { filterAndSortHeroes } from "./heroRanking";
+export type { HeroSort, HeroRankingOptions } from "./heroRanking";
+export { reorderArray } from "./selectionUtils";
+export {
+  calculateHeroUsage,
+  calculateWinRate,
+  findBestHistoricalTeam,
+} from "./combatStats";
+export type { WinRateResult, BestWinTeamResult } from "./combatStats";
