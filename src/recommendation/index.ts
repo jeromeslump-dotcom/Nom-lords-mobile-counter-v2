c2f707891fec721692cac2c8fcde9cbddeec53b1
jeromeslump-dotcom <jeromeslump@gmail.com>
2026-08-21 20:40:37 +0200

    Add recommendation module entrypoint

---
+++ src/recommendation/index.ts
@@ -0,0 +1,6 @@
+export { RECOMMENDATION_CONFIG } from "./recommendationConfig";
+export type { RecommendationConfig } from "./recommendationConfig";
+export { findFiveHeroMatchups } from "./matchupAnalysis";
+export type { MatchupRecord } from "./matchupAnalysis";
+export { getHistoricalTeamsAgainst } from "./teamHistoryAnalysis";
+export type { HistoricalTeamRecord } from "./teamHistoryAnalysis";
